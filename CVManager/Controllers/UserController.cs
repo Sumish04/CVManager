ef1de281f89b9ba90a8fbf483f21fdb0e7ed4428
Sumish <sumish@ix.co.za>
2024-04-21 19:02:00 +0200

    Added Images to profiles. Changed the look and feel on the dashboard. Added the ability to view the cv in the browser.

---
+++ CVManager/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVManager.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index(int? userID)
+        {
+            ViewBag.UserID = userID;
+            return View();
+        }
+    }
+}
